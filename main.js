2d286df77dfdf59fb88659bcfeb8c76823ea9527
Adu Atiloye <hartiloye2002@gmail.com>
2024-03-16 11:50:23 +0100

    style and js update

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
 // To switch to comfirmation page.
-document.getElementById('button').addEventListener('click', function() {
+document.getElementById('button').addEventListener('click', function(event) {
     document.querySelector('.main-cont').style.display = 'none';
     document.querySelector('.sub-cont').style.display = 'flex';
+    event.preventDefault();
+
 });
 
-// When dismiss message is clicked
+// When dismiss message is clicked.
 document.getElementById('dismissButton').addEventListener('click', function () {
   alert("Message Dismissed");
   location.reload();
 })
 
-// Email display
+// Email display.
 document.getElementById('button').addEventListener('click', function() {
     const inputValue = document.getElementById('email').value;
     document.getElementById('inputEmail').innerText = inputValue;
 });
 
-
-// Check if the email is valid or not.
+// Email validation.
